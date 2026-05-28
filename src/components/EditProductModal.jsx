dd6c8d2cc8aa484f997c0bd4a57d9b5115a3f354
sunjewellerydpm <websitesunjewels@gmail.com>
2026-05-28 22:54:33 +0530

    Update EditProductModal.jsx

--- src/components/EditProductModal.jsx
+++ src/components/EditProductModal.jsx
@@ -145,6 +145,7 @@ const EditProductModal = ({ product, onClose, onSave }) => {
                                     <option value="ladies-rings">Ladies Rings</option>
                                     <option value="gents-rings">Gents Rings</option>
                                     <option value="studs-earrings">Studs & Earrings</option>
+                                    <option value="chokers">Chokers</option>
                                 </select>
                             </div>
                             <div className="form-group">
